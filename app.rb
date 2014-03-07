ee8dfd16bdd648cb31bd04afd4c39c6f4bf67675
Oscar Gonzalez <oscar@tacitknowledge.com>
2014-03-07 14:53:41 -0500

    Adding pony to send email

--- app.rb
+++ app.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
+require 'pony'
 
 get '/' do
   "Hola Enfermera"
+end
+
+post '/push' do
+
+options = {
+  :to => 'oscar-heroku@tacitknowledge.com',
+  :from => 'oscar@tacitknowledge.com',
+  :subject => 'New commit',
+  :body => 'There is a new commit' + params[:head],
+  :via => :smtp,
+  :via_options => {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'tacitknowledge.com',
+    :user_name => 'ogonzalez',
+    :password => 'ja-pazy-lo-ren',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+    }
+  } 
+
+  Pony.mail(options)
+
 end
